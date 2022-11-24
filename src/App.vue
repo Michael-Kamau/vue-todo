31938d27f5aa54b5e4be48d5888ee108fdacceb3
Michael Kamau <kamau.karitu@gmail.com>
2022-11-24 18:38:59 +0300

    Initial project setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <div>
+    <h1 className="text-3xl font-bold ">
+
+      Hello world!
+    </h1>
+  </div>
+  <Home msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+</script>
+
+<template>
+  <h1 class="bg-red-100">Inner Component</h1>
+
+</template>
+
+<style scoped>
+
+</style>
