87520255df1699a3b00a922a515682096bda0e2c
Michael Kamau <kamau.karitu@gmail.com>
2022-11-25 23:19:32 +0300

    Theme Support - Enable support for dark and light theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,9 +7,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'desktop-light': "url('/src/assets/images/bg-desktop-dark.jpg')",
+        'desktop-light': "url('/src/assets/images/bg-desktop-light.jpg')",
+        'desktop-dark': "url('/src/assets/images/bg-desktop-dark.jpg')",
+        'mobile-light': "url('/src/assets/images/BG-MOBILE-LIGHT.jpg')",
+        'mobile-dark': "url('/src/assets/images/BG-MOBILE-DARK.jpg')",
+        'moon-icon': "url('/src/assets/images/ICON-MOON.SVG')",
+        'sun-icon': "url('/src/assets/images/ICON-SUN.SVG')",
       }
     },
   },
   plugins: [],
+
+  darkMode:'class'
 }
